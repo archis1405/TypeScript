f71ad763815a653ac16a879c0c1a3ede53f32565
Archisman <archismandas1405@gmail.com>
2024-12-20 19:01:08 +0530

    First commit

---
+++ Types.ts
@@ -0,0 +1,27 @@
+// Types in Typescript 
+/*
+
+    Therse are the types supported by TypeScript ::
+        1. String -> store texts
+        2. numbers -> for integers and real numbers
+        3. boolean 
+        4. undefined 
+        5. null 
+        6. bigint
+        7. symbol -> types of special object that are going to mimic like a String
+
+        all the 7 aare primitive types in TypeScript
+
+*/
+
+// Defining a variable and storing a value
+
+//  var <variable_name> : <type> = <value>
+// Example :: 
+
+let id : number = 5;
+console.log(id);
+
+// This is known as Type-annotation or Type-Signature
+
+let names : String = "Oswaldd" // TS will usderstand name will always store a string 
