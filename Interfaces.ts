a3daf36f1671c77dcea48e813e9132b24f7e57c5
Archisman <archismandas1405@gmail.com>
2024-12-20 21:30:29 +0530

    Types in typeScript compleated

---
+++ Interfaces.ts
@@ -0,0 +1,25 @@
+/**
+ * How to define types for objects ?
+ *  1. Classes -> data members and member functions  
+ *  2. Interfaces 
+ *  
+ *  Using Classes ::
+ *      class Car{
+ *          name : string 
+ *          constructor (name : string){
+ *              this.name = name;
+ *          }
+ * 
+ *          display(){
+ *              console.log(this.name);
+ *          }
+ *      };
+ * 
+ *  Classes contains the implementation of the member functions as well where as interfaces donot implement the member functions
+ *  
+ *  INTERFACE::
+ *      Interface is basically a contract . 
+ *      Interfaces are used to define the structure of an object in Typescript.
+ * 
+ *      i
+ */
--- Types.ts
+++ Types.ts
@@ -24,4 +24,37 @@ console.log(id);
 
 // This is known as Type-annotation or Type-Signature
 
-let names : String = "Oswaldd" // TS will usderstand name will always store a string 
+let names : string = "Oswaldd" // TS will usderstand name will always store a string 
+
+// Union of Types 
+let userId : number | string ="26";
+userId = 10 ;
+/*
+    Union means that for a particular variable there might exist more than one type of values 
+*/
+
+
+// ARRAYS in TypeScript
+
+/**
+ * Defining an array :: -->> let <variable_name> : <type>[] = [val1, val2,val3];
+ * 
+ *  
+ */
+
+let ids : number[] = [1,2,3,4,5];
+console.log(ids);
+
+/**
+ * NOTE - Javascript has hetrogeneous arrays but typescript do not have heterogeneous arrays 
+ */
+
+// Defining Hetrogeneous arrays
+
+let hetroarray : any[] = [1,2,false,"hi",null];
+
+// Union of datatypes for arrays 
+
+let data : (number | string | boolean)[] = [2,3,true,"hello"];
+
+
